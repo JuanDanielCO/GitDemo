5512688992bfce23cb7de6ba04f19e55099c3c8c
Daniel <ing.daniel.castano@gmail.com>
2021-02-01 12:01:36 -0500

    First Commit

---
+++ src/Parents.java
@@ -0,0 +1,12 @@
+public class Parents {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Methods m = new Methods();
+		System.out.println(m.ValidateHeader());
+		//Class object.methodname
+	}
+
+}
